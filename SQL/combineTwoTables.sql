5f6c51c346aa5596b6b00acfa5936bc68a1f928d
Dravonosec <dravonosec@yandex.ru>
2024-12-13 22:57:15 +0500

    Задача на SQL: Combine Two Tables

---
+++ SQL/combineTwoTables.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/combine-two-tables/?envType=problem-list-v2&envId=database
+
+SELECT firstName, lastName, city, state
+FROM Person
+LEFT JOIN Address ON Person.personId = Address.personId 
