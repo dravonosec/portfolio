e68002b9e12ebdb2f4b1cbd76124636e30f13b1e
Dravonosec <dravonosec@yandex.ru>
2024-12-20 11:29:26 +0500

    Refactoring Rebase

--- SQL/findCustomerReferee.sql
+++ SQL/findCustomerReferee.sql
@@ -3,4 +3,4 @@ SET NOCOUNT ON;
 -- https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
 
 SELECT name FROM Customer
-WHERE NOT referee_id = 2 OR referee_id IS NULL
+WHERE NOT referee_id = 2 OR referee_id IS NULL;
